e5b3fb9bbbb8ea3cad9e108094448115f0ef0336
tremoral <tremor_al@hotmail.com>
2017-05-27 01:24:34 -0500

    cambios al tanque

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -17,6 +17,37 @@ void ATankPlayerController::BeginPlay()
 	
 }
 
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	//start the tank moving the varrel so tha a show woult it where
+	//the crosshair intersects the world
+	AimTowardsCrosshair();
+	//UE_LOG(LogTemp, Warning, TEXT("PlayerController ticking"));
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) {
+		return;
+	}
+	FVector HitLocation;
+	if (GetSightRayHitLocation(HitLocation))//has side effect is going to line trace
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), *HitLocation.ToString());
+		//TODO tell controlled tank to aim at that point
+	}
+}
+
+bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) const
+{
+	//OutHitLocation = FVector(1.0);
+	//find the crosshair position
+	//deproject screen position of the cursos to a world direction
+	//line trace a
+	return true;
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -27,7 +27,11 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	
 public:
 	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+	void AimTowardsCrosshair();
+	
 private:
 	ATank* GetControlledTank() const;
+	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
 
 };
